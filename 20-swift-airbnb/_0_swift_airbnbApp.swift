56661948e703b54375969a2fc2c3f7c380eadca2
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-26 15:16:11 +0800

    Initial Commit

---
+++ 20-swift-airbnb/_0_swift_airbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  _0_swift_airbnbApp.swift
+//  20-swift-airbnb
+//
+//  Created by Liu Ziyi on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct _0_swift_airbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
