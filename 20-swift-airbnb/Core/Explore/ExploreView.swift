4d852ec38280fb4f159d308e7471795ec71cd49d
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-27 10:15:04 +0800

    - Custom Search Bar

--- 20-swift-airbnb/Core/Explore/ExploreView.swift
+++ 20-swift-airbnb/Core/Explore/ExploreView.swift
@@ -11,6 +11,8 @@ struct ExploreView: View {
     var body: some View {
         NavigationStack {
             ScrollView {
+                SearchAndFilterBar()
+                
                 LazyVStack(spacing: 32) {
                     ForEach(0...10, id: \.self) { listing in
                         ListingItemView()
---
+++ 20-swift-airbnb/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,50 @@
+//
+//  SearchAndFilterBar.swift
+//  20-swift-airbnb
+//
+//  Created by Liu Ziyi on 27/10/23.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+            
+            VStack(alignment: .leading, spacing: 2) {
+                Text("Where to?")
+                    .font(.footnote)
+                    .fontWeight(.semibold)
+                
+                Text("Anywhere - Any Week - Add guests")
+                    .font(.caption2)
+                    .foregroundStyle(.gray)
+            }
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                Image(systemName: "line.3.horizontal.decrease.circle")
+                    .foregroundStyle(.black)
+            }
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 10)
+        .overlay {
+            Capsule()
+                .stroke(lineWidth: 0.5)
+                .foregroundStyle(Color(.systemGray4))
+                .shadow(color: .black.opacity(0.4), radius: 2)
+        }
+        .padding()
+    }
+}
+
+struct SearchAndFilterBar_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchAndFilterBar()
+    }
+}
