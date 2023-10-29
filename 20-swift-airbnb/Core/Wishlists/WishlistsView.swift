631615f32d17fdc4fced0e7c6764a94c6aaf9cb7
ziyiliu02 <ziyi.liu02@gmail.com>
2023-10-29 11:20:49 +0800

    - Wishlists View

---
+++ 20-swift-airbnb/Core/Wishlists/WishlistsView.swift
@@ -0,0 +1,46 @@
+//
+//  WishlistsView.swift
+//  20-swift-airbnb
+//
+//  Created by Liu Ziyi on 29/10/23.
+//
+
+import SwiftUI
+
+struct WishlistsView: View {
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 32) {
+                VStack(alignment: .leading, spacing: 4) {
+                    Text("Log in to view your wishlists")
+                        .font(.headline)
+                    
+                    Text("You can create, view or edit wishlists once you've logged in")
+                        .font(.footnote)
+                }
+                
+                Button {
+                    print("Log in")
+                } label: {
+                    Text("Log in")
+                        .foregroundStyle(.white)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .frame(width: 360, height: 48)
+                        .background(.pink)
+                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                }
+                
+                Spacer()
+            }
+            .padding()
+            .navigationTitle("Wishlists")
+        }
+    }
+}
+
+struct WishlistsView_Previews: PreviewProvider {
+    static var previews: some View {
+        WishlistsView()
+    }
+}
